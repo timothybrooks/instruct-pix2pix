c61d8a4b7c0ff3a253dca454c788f2f32242d708
Meatfucker <74834323+Meatfucker@users.noreply.github.com>
2023-01-21 21:35:05 -0500

    Update download_checkpoints.bat

--- scripts/download_checkpoints.bat
+++ scripts/download_checkpoints.bat
@@ -1,2 +1,2 @@
 mkdir ..\checkpoints
-curl "http://instruct-pix2pix.eecs.berkeley.edu/instruct-pix2pix-00-22000.ckpt" --output "..\checkpoints\instruct-pix2pix-00-22000"
+curl "http://instruct-pix2pix.eecs.berkeley.edu/instruct-pix2pix-00-22000.ckpt" --output "..\checkpoints\instruct-pix2pix-00-22000.ckpt"
