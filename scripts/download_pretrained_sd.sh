6dfdfbfb19b87fe152485a66cf81e1390a2386e9
amcox886 <155285242+amcox886@users.noreply.github.com>
2024-03-03 21:41:05 +0000

    update to pretrained data
    Okay, I went ahead and just updated that script but checked if HF_HUB_ENABLE_HF_TRANSFER was set first otherwise just left the original curl in place.

--- scripts/download_pretrained_sd.sh
+++ scripts/download_pretrained_sd.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+DATA_DIR="${SCRIPT_DIR}/../stable_diffusion/models/ldm/stable-diffusion-v1"
 
-mkdir -p $SCRIPT_DIR/../stable_diffusion/models/ldm/stable-diffusion-v1
-curl -L https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.ckpt -o $SCRIPT_DIR/../stable_diffusion/models/ldm/stable-diffusion-v1/v1-5-pruned-emaonly.ckpt
-curl -L https://huggingface.co/stabilityai/sd-vae-ft-mse-original/resolve/main/vae-ft-mse-840000-ema-pruned.ckpt -o $SCRIPT_DIR/../stable_diffusion/models/ldm/stable-diffusion-v1/vae-ft-mse-840000-ema-pruned.ckpt
+mkdir -p ${DATA_DIR}
+
+if ( [ $HF_HUB_ENABLE_HF_TRANSFER ] ); then
+	echo "got into if"
+	# assumes that huggingface-cli and hf-transfer are installed and HF_HUB_ENABLE_HF_TRANSFER=1
+	# in environment globals. It also requires you have a HF_TOKEN set.
+	# NB although faster, hf-transfer doesn't support debugging or resuming partial downloads
+	# see https://huggingface.co/docs/huggingface_hub/en/guides/download#download-from-the-cli
+	huggingface-cli download --local-dir $DATA_DIR "runwayml/stable-diffusion-v1-5" v1-5-pruned-emaonly.ckpt
+	huggingface-cli download --local-dir $DATA_DIR "stabilityai/sd-vae-ft-mse-original" vae-ft-mse-840000-ema-pruned.ckpt
+else
+	echo "got into else"
+	curl -L https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.ckpt -o $DATA_DIR/v1-5-pruned-emaonly.ckpt
+	curl -L https://huggingface.co/stabilityai/sd-vae-ft-mse-original/resolve/main/vae-ft-mse-840000-ema-pruned.ckpt -o $DATA_DIR/vae-ft-mse-840000-ema-pruned.ckpt
+fi 
+
