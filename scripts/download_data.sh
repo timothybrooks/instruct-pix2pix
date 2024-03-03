d421bd8ddf2746c8be91d2679542519aed6ddb57
amcox886 <155285242+amcox886@users.noreply.github.com>
2024-03-03 20:57:25 +0000

    faster downloads with wget
    Updated scripts in /scripts. Note that only download_checkpoints.sh and download_data.sh are included. To update download_pretrained_sd.sh, it would be better to do a version where huggingface-cli and hf-transfer are required, as this would use the native download wrappers.

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -6,8 +6,8 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 mkdir -p $SCRIPT_DIR/../data
 
 # Copy text datasets
-wget -q --show-progress http://instruct-pix2pix.eecs.berkeley.edu/gpt-generated-prompts.jsonl -O $SCRIPT_DIR/../data/gpt-generated-prompts.jsonl
-wget -q --show-progress http://instruct-pix2pix.eecs.berkeley.edu/human-written-prompts.jsonl -O $SCRIPT_DIR/../data/human-written-prompts.jsonl
+wget -nc -c -q --show-progress http://instruct-pix2pix.eecs.berkeley.edu/gpt-generated-prompts.jsonl -O $SCRIPT_DIR/../data/gpt-generated-prompts.jsonl
+wget -nc -c -q --show-progress http://instruct-pix2pix.eecs.berkeley.edu/human-written-prompts.jsonl -O $SCRIPT_DIR/../data/human-written-prompts.jsonl
 
 # If dataset name isn't provided, exit. 
 if [ -z $1 ] 
@@ -17,7 +17,13 @@ fi
 
 # Copy dataset files
 mkdir $SCRIPT_DIR/../data/$1
-wget -A zip,json -R "index.html*" -q --show-progress -r --no-parent http://instruct-pix2pix.eecs.berkeley.edu/$1/ -nd -P $SCRIPT_DIR/../data/$1/
+wget -nc -A zip,json -R "index.html*" -q --show-progress -r --no-parent -c http://instruct-pix2pix.eecs.berkeley.edu/$1/ -nd -P $SCRIPT_DIR/../data/$1/
+
+if [ -z $2 -a ( $2="master" )]; then
+	True
+else
+	exit 2
+fi
 
 # Unzip to folders
 unzip $SCRIPT_DIR/../data/$1/\*.zip -d $SCRIPT_DIR/../data/$1/
